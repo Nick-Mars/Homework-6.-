a2b5c32fd8c6fb336dd4906752bc1426339eda64
Nick-Mars <nickshootnick@gmail.com>
2021-10-16 22:35:04 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// //Task #1
+// const lin1 = Boolean(Number(`10`)) + 1; //2,number
+// const lin2 = `sub` + Number(false); //sub0, string
+// const lin3 = 16 * `      91    `; //1456, numer
+// const lin4 = true-70; //-69, numer 
+// const lin5 = Number(1 + String(1)) + 1; // 12, number
+
+// console.log (typeof(lin1));
+// console.log (typeof(lin2));
+// console.log (typeof(lin3));
+// console.log (typeof(lin4));
+// console.log (typeof(lin5));
+
+// //Task #2
+// const numbSecond = 60 * 60;
+// const numbHours = +prompt ('Укажте количество часов');
+// const count = numbSecond * numbHours;
+// alert (count);
+
+//Task #3
+// let num = 1;
+// num = num + 12;
+// num = num - 14;
+// num = num * 5;
+// num = num / 7;
+// num = num + 1;
+// num = num - 1;
+// alert(num);
+
+//Переделка:
+
+// let num = 1;
+// num += 12;
+// num -= 14;
+// num *= 5;
+// num /= 7
+// ++num;
+// --num;
+// alert(num);
